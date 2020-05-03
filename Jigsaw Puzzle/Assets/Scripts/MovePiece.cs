dd2ae10433666c91a5d7d41cef9248ba4d493f33
Jacob Ilin <me@jacobilin.com>
2020-05-03 02:35:06 -0600

    Puzzle created

--- Jigsaw Puzzle/Assets/Scripts/MovePiece.cs
+++ Jigsaw Puzzle/Assets/Scripts/MovePiece.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class MovePiece : MonoBehaviour
 {
 
-    public string pieceStatus = "";
+    public string pieceStatus = "idle";
 
     public Transform edgeParticles;
 
+    public KeyCode placePiece;
 
+    public string checkPlacement;
 
     // Start is called before the first frame update
     void Start()
@@ -20,23 +22,45 @@ public class MovePiece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (pieceStatus != "locked")
+        if (pieceStatus == "pickedup")
         {
             Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
             transform.position = objPosition;
         }
-        
+
+        if (Input.GetKeyDown(placePiece))
+        {
+            checkPlacement = "y";
+        }
     }
 
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.name == gameObject.name)
+        if (other.gameObject.name == gameObject.name && checkPlacement == "y")
         {
+            other.GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<BoxCollider2D>().enabled = false;
+
             transform.position = other.gameObject.transform.position;
             pieceStatus = "locked";
             Instantiate(edgeParticles, other.gameObject.transform.position, edgeParticles.rotation);
+            checkPlacement = "n";
+
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
         }
+
+        if (other.gameObject.name != gameObject.name && checkPlacement == "y")
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0.5f);
+            checkPlacement = "n";
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        pieceStatus = "pickedup";
+        checkPlacement = "n";
     }
 }
