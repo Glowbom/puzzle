17fcf502ae0deaeb3d5adf1eb200c54d15f73551
Jacob Ilin <me@jacobilin.com>
2020-05-04 15:34:20 -0600

    Scrolling inventory

--- Jigsaw Puzzle/Assets/Scripts/MovePiece.cs
+++ Jigsaw Puzzle/Assets/Scripts/MovePiece.cs
@@ -10,8 +10,12 @@ public class MovePiece : MonoBehaviour
     public Transform edgeParticles;
 
     public KeyCode placePiece;
+    public KeyCode returnInv;
 
     public string checkPlacement;
+
+    public float yDiff;
+    public Vector2 invPos;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +26,8 @@ public class MovePiece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        invControl();
+
         if (pieceStatus == "pickedup")
         {
             Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
@@ -63,5 +69,28 @@ public class MovePiece : MonoBehaviour
         pieceStatus = "pickedup";
         checkPlacement = "n";
         GetComponent<Renderer>().sortingOrder = 10;
+        invPos = transform.position;
     }
+
+    void invControl()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && pieceStatus != "locked")
+        {
+            transform.position = new Vector2(transform.position.x - 0.4f, -3.7f);
+            yDiff -= 0.4f;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") < 0 && pieceStatus != "locked")
+        {
+            transform.position = new Vector2(transform.position.x + 0.4f, -3.7f);
+            yDiff += 0.4f;
+        }
+
+        if (Input.GetKeyDown(returnInv) && pieceStatus == "pickedup")
+        {
+            transform.position = new Vector2(invPos.x + yDiff, -3.7f);
+            pieceStatus = ""; 
+        }
+    }
+
 }
