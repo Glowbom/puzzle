0f80e1bdc62dd8cd71a5fdd751b3094d80db86b9
Jacob Ilin <me@jacobilin.com>
2020-05-09 23:31:11 -0600

    Puzzle pieces rotation, long button clicks

---
+++ Jigsaw Puzzle/Assets/Scripts/LongClick.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class LongClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown = false;
+    private float pointerDownTimer;
+
+    public float requiredHoldTime;
+    public UnityEvent onLongClick;
+    public UnityEvent onNormalClick;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        reset();
+    }
+
+    void reset()
+    {
+        if (pointerDownTimer < requiredHoldTime && onNormalClick != null)
+        {
+            onNormalClick.Invoke();
+        }
+
+        pointerDown = false;
+        pointerDownTimer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pointerDown)
+        {
+            pointerDownTimer += Time.deltaTime;
+            if (pointerDownTimer >= requiredHoldTime)
+            {
+                if (onLongClick != null)
+                {
+                    onLongClick.Invoke();
+
+                    reset();
+                }
+            }
+        }
+    }
+}
---
+++ Jigsaw Puzzle/Assets/Scripts/Puzzle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle : MonoBehaviour
+{
+    public Button[] buttons;
+
+    public void puzzleClicked(int index)
+    {
+        buttons[index].gameObject.transform.eulerAngles = new Vector3(
+            buttons[index].gameObject.transform.eulerAngles.x,
+            buttons[index].gameObject.transform.eulerAngles.y,
+            buttons[index].gameObject.transform.eulerAngles.z + 90
+        );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
