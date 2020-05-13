ad2d23c2c9e03d14dbda422462e99f2a97c44b15
Jacob Ilin <me@jacobilin.com>
2020-05-12 20:47:02 -0600

    Winning logic

--- Jigsaw Puzzle/Assets/Scripts/Puzzle.cs
+++ Jigsaw Puzzle/Assets/Scripts/Puzzle.cs
@@ -10,46 +10,101 @@ public class Puzzle : MonoBehaviour
 
     public LongClick[] buttons;
     public LongClick[] mediumButtons;
+    public Toggle toggle;
 
     public GameObject easyGame;
     public GameObject mediumGame;
+    public GameObject victoryView;
 
     public bool rotate = true;
+    public bool isGameInProgress = false;
 
 
     private int firstClickedIndex = -1;
     private int[] dataEasy;
     private int[] dataMedium;
-    private int[] dataHard;
 
     private string keyMedium = "sanfranciso_medium";
     private string keyEasy = "sanfranciso";
 
-
-    public void puzzleLongClicked(int index)
+    private void checkWinninState()
     {
+        bool won = true;
         if (easyGame.activeInHierarchy)
         {
-            buttons[index].gameObject.transform.eulerAngles = new Vector3(
-            buttons[index].gameObject.transform.eulerAngles.x,
-            buttons[index].gameObject.transform.eulerAngles.y,
-            buttons[index].gameObject.transform.eulerAngles.z + 90
-            );
+            for(int i = 0; i < dataEasy.Length; i++)
+            {
+                if (dataEasy[i] != i || buttons[i].gameObject.transform.eulerAngles.z >= 90f)
+                {
+                    Debug.Log("i = " + i +", dataEasy[i] != " + i + ", buttons[i].gameObject.transform.eulerAngles.z = " + buttons[i].gameObject.transform.eulerAngles.z +
+                        " !Mathf.Approximately(buttons[i].gameObject.transform.eulerAngles.z, 0f) = " + !Mathf.Approximately(buttons[i].gameObject.transform.eulerAngles.z, 0f));
+                    won = false;
+                    break;
+                }
+            }
         }
 
         if (mediumGame.activeInHierarchy)
         {
-            mediumButtons[index].gameObject.transform.eulerAngles = new Vector3(
-            mediumButtons[index].gameObject.transform.eulerAngles.x,
-            mediumButtons[index].gameObject.transform.eulerAngles.y,
-            mediumButtons[index].gameObject.transform.eulerAngles.z + 90
-            );
+            for (int i = 0; i < dataMedium.Length; i++)
+            {
+                if (dataMedium[i] != i || mediumButtons[i].gameObject.transform.eulerAngles.z >= 90f)
+                {
+                    won = false;
+                    break;
+                }
+            }
         }
 
+        if (won)
+        {
+            victoryView.SetActive(true);
+            isGameInProgress = false;
+        }
+    }
+
+
+    public void puzzleLongClicked(int index)
+    {
+        if (isGameInProgress)
+        {
+            if (easyGame.activeInHierarchy)
+            {
+                buttons[index].gameObject.transform.eulerAngles = new Vector3(
+                buttons[index].gameObject.transform.eulerAngles.x,
+                buttons[index].gameObject.transform.eulerAngles.y,
+                buttons[index].gameObject.transform.eulerAngles.z + 90
+                );
+            }
+
+            if (mediumGame.activeInHierarchy)
+            {
+                mediumButtons[index].gameObject.transform.eulerAngles = new Vector3(
+                mediumButtons[index].gameObject.transform.eulerAngles.x,
+                mediumButtons[index].gameObject.transform.eulerAngles.y,
+                mediumButtons[index].gameObject.transform.eulerAngles.z + 90
+                );
+            }
+        }
+
+        checkWinninState();
     }
 
     public void puzzleClicked(int index)
     {
+        if (!isGameInProgress)
+        {
+            if (easyGame.activeInHierarchy)
+            {
+                easyPressed();
+            }
+            else if (mediumGame.activeInHierarchy)
+            {
+                mediumPressed();
+            }
+            return;
+        }
+
         if (firstClickedIndex == -1)
         {
             firstClickedIndex = index;
@@ -90,9 +145,12 @@ public class Puzzle : MonoBehaviour
                 }
 
                 firstClickedIndex = -1;
+                checkWinninState();
                 return;
             }
             swap(index);
+
+            checkWinninState();
         }
     }
 
@@ -145,7 +203,7 @@ public class Puzzle : MonoBehaviour
         }
 
 
-            if (easyGame.activeInHierarchy)
+        if (easyGame.activeInHierarchy)
         {
             int firstData = dataEasy[firstClickedIndex];
             int lastData = dataEasy[index];
@@ -208,32 +266,57 @@ public class Puzzle : MonoBehaviour
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void restart()
     {
-        loadResources();
+        isGameInProgress = false;
+        victoryView.SetActive(false);
+        //easyGame.SetActive(true);
+        //mediumGame.SetActive(false);
 
-        easyGame.SetActive(true);
-        mediumGame.SetActive(false);
+        firstClickedIndex = -1;
 
         dataEasy = new int[buttons.Length];
         for (int i = 0; i < buttons.Length; i++)
         {
             dataEasy[i] = i;
+
+            buttons[i].gameObject.transform.eulerAngles = new Vector3(
+                buttons[i].gameObject.transform.eulerAngles.x,
+                buttons[i].gameObject.transform.eulerAngles.y,
+                0f
+            );
         }
 
         dataMedium = new int[mediumButtons.Length];
         for (int i = 0; i < mediumButtons.Length; i++)
         {
             dataMedium[i] = i;
+
+
+            mediumButtons[i].gameObject.transform.eulerAngles = new Vector3(
+                mediumButtons[i].gameObject.transform.eulerAngles.x,
+                mediumButtons[i].gameObject.transform.eulerAngles.y,
+                0f
+            );
         }
 
 
         refreshUi();
     }
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadResources();
+
+        restart();
+    }
+
     public void mediumPressed()
     {
+        isGameInProgress = true;
+        victoryView.SetActive(false);
+        rotate = toggle.isOn;
         easyGame.SetActive(false);
         mediumGame.SetActive(true);
 
@@ -257,6 +340,13 @@ public class Puzzle : MonoBehaviour
                     mediumButtons[i].gameObject.transform.eulerAngles.y,
                     randomRotation * 90f
                 );
+            } else
+            {
+                mediumButtons[i].gameObject.transform.eulerAngles = new Vector3(
+                mediumButtons[i].gameObject.transform.eulerAngles.x,
+                mediumButtons[i].gameObject.transform.eulerAngles.y,
+                0f
+                );
             }
 
         }
@@ -267,6 +357,9 @@ public class Puzzle : MonoBehaviour
 
     public void easyPressed()
     {
+        isGameInProgress = true;
+        victoryView.SetActive(false);
+        rotate = toggle.isOn;
         easyGame.SetActive(true);
         mediumGame.SetActive(false);
 
@@ -289,6 +382,13 @@ public class Puzzle : MonoBehaviour
                     buttons[i].gameObject.transform.eulerAngles.x,
                     buttons[i].gameObject.transform.eulerAngles.y,
                     randomRotation * 90f
+                );
+            } else
+            {
+                buttons[i].gameObject.transform.eulerAngles = new Vector3(
+                buttons[i].gameObject.transform.eulerAngles.x,
+                buttons[i].gameObject.transform.eulerAngles.y,
+                0f
                 );
             }
 
